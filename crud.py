80e8920b98789077bd5039c5898308e13559971d
bartop <bartosz.dalecki@ziptie.dev>
2023-03-16 23:18:02 +0100

    crud update method

--- crud.py
+++ crud.py
@@ -40,6 +40,21 @@ class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         db.refresh(db_obj)
         return db_obj
 
+    def update(self, db: Session, *, db_obj: ModelType,
+               obj_in: UpdateSchemaType | Dict[str, Any]) -> ModelType:
+        obj_data = jsonable_encoder(db_obj)
+        if isinstance(obj_in, dict):
+            update_data = obj_in
+        else:
+            update_data = obj_in.dict(exclude_unset=True)
+            for field in obj_data:
+                if field in update_data:
+                    setattr(db_obj, field, update_data[field])
+            db.add(db_obj)
+            db.commit()
+            db.refresh(db_obj)
+            return db_obj
+
     def delete(self, db: Session, *, obj_id: int) -> ModelType:
         obj = select(self.model).where(self.model.id == obj_id)
         db.delete(obj)
