0c7e4712be93f375e5d185d4456109ad96874f90
bartosz <bartosz.dalecki@ziptie.dev>
2023-04-18 20:09:42 +0200

    tests & validators for watering, minor changes

---
+++ tests/unit/test_waterings/test_watering_model.py
@@ -0,0 +1,25 @@
+import pytest
+
+from datetime import datetime
+from routers.watering.models import Watering
+
+
+def test_model_correct():
+    # correct model
+    assert Watering(
+        id=1,
+        plant_id=2,
+        timestamp=datetime(2023, 4, 12, 18, 11, 29, 275000),
+        fertilizer=True,
+        user_id=3
+    )
+
+
+def test_model_wrong():
+    assert Watering(
+        id='string',
+        plant_id=2,
+        timestamp=datetime(2023, 4, 12, 18, 11, 29, 275000),
+        fertilizer=True,
+        user_id=3
+    )
