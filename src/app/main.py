eab2ac9fd7a7fda9400e177d496fe4b0bc72254c
bartosz <badalecki@gmail.com>
2023-11-16 20:42:10 +0100

    cors

--- src/app/main.py
+++ src/app/main.py
@@ -25,7 +25,10 @@ app.add_middleware(CORSMiddleware,
                                   'http://localhost',
                                   'http://localhost:3000',
                                   'http://localhost:8000',
-                                  'http://localhost:8080'],
+                                  'http://localhost:8080',
+                                  'http://127.0.0.1:8080',
+                                  'http://0.0.0.0:8080'
+                                  ],
                    allow_credentials=True,
                    allow_methods=['*'],
                    allow_headers=['*'])
