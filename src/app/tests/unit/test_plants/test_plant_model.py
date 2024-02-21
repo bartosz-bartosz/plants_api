850701be203b3c2fd72f5b4eb71eba3a5a9bbc1f
bartosz <badalecki@gmail.com>
2024-02-21 19:18:12 +0100

    simpler tests - plant model

--- src/app/tests/unit/test_plants/test_plant_model.py
+++ src/app/tests/unit/test_plants/test_plant_model.py
@@ -4,7 +4,17 @@ from datetime import datetime
 from app.routers.plant.models import Plant
 
 
-def test_name():
+valid_plant_data = {
+    "user_id": 1,
+    "name": "plant name",
+    "acquire_time": datetime.now,
+    "is_alive": 1,
+    "species": "plant species",
+    "watering_frequency": 7,
+}
+
+
+def test_valid_plant():
     # Correct model
     assert Plant(
         user_id=1,
@@ -15,36 +25,19 @@ def test_name():
         watering_frequency=7,
     )
 
+
+@pytest.mark.parametrize(
+    "key, value",
+    [
+        ("name", ""),
+        ("name", True),
+        ("name", "a"),
+        ("name", "".join(["i" for _ in range(201)])),
+    ],
+)
+def test_invalid_plant(key, value):
     # Wrong models
-    with pytest.raises(ValueError):
-        # name empty
-        name_fail = Plant(
-            user_id=1,
-            name="",
-            acquire_time=datetime.now,
-            is_alive=1,
-            species="plant species",
-            watering_frequency=7,
-        )
-
-    with pytest.raises(ValueError):
-        # name too short
-        name_fail = Plant(
-            user_id=1,
-            name="a",
-            acquire_time=datetime.now,
-            is_alive=1,
-            species="plant species",
-            watering_frequency=7,
-        )
-
-    with pytest.raises(ValueError):
-        # name too long
-        name_fail = Plant(
-            user_id=1,
-            name="".join(["i" for _ in range(201)]),
-            acquire_time=datetime.now,
-            is_alive=1,
-            species="plant species",
-            watering_frequency=7,
-        )
+    data_set = valid_plant_data.copy()
+    data_set[key] = value
+    with pytest.raises((ValueError, TypeError)):
+        Plant(**data_set)
