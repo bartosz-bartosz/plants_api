5843cb86171c999e5c25267213b9228f50c742c8
bartosz <badalecki@gmail.com>
2024-02-24 13:31:01 +0100

    custom schema in test database

--- src/app/tests/unit/test_plants/test_plants_routers.py
+++ src/app/tests/unit/test_plants/test_plants_routers.py
@@ -1,16 +1,20 @@
 from datetime import datetime
 import pytest
 
+from app.routers.plant.models import Plant
+
 
 def create_plant(test_sesion):
-    plant = m.Plant(name="test_plant",
-                    acquire_time=datetime.now(),
-                    species="test_species",
-                    watering_frequency=7,
-                    user_id=1)
+    plant = Plant(name="test_plant",
+                  acquire_time=datetime.now(),
+                  species="test_species",
+                  watering_frequency=7,
+                  user_id=1)
     test_session.add(plant)
     test_session.commit()
     return plant
+
+@pytest.fixture
 def test_token(test_client, test_user):
     response = test_client.post('/token',
                            data=test_user)
@@ -20,23 +24,21 @@ def test_token(test_client, test_user):
     return token
 
 
-def test_create_plant(test_client, test_user):
-    token = test_token(test_client, test_user)
+def test_create_plant(test_client, test_user, test_token):
     response = test_client.post("/plant",
                                 json={"name": "test_plant",
                                     "acquire_time": 
                                       datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                                     "species": "test_species",
                                     "watering_frequency": 7},
-                                headers={"Authorization": f"Bearer {token}"})
+                                headers={"Authorization": f"Bearer {test_token}"})
     assert response.status_code == 201
     print(response.json())
 
 
-def test_read_plant(test_client, test_user):
-    test_create_plant(test_client, test_user)
-    token = test_token(test_client, test_user)
+def test_read_plant(test_client, test_user, test_token):
+    test_create_plant(test_client, test_user, test_token)
     response = test_client.get("/plant/1",
-                               headers={"Authorization": f"Bearer {token}"})
+                               headers={"Authorization": f"Bearer {test_token}"})
     assert response.status_code == 200
     assert response.json()["name"] == "test_plant"
